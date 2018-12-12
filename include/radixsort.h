e35326490ff735b1707127483588cec78b68a689
DFEscrofani <escrofanidt@gmail.com>
2018-12-12 14:21:32 -0800

    edit 1

--- include/radixsort.h
+++ include/radixsort.h
@@ -1,6 +1,15 @@
 #include <cstdint>
+#include <iostream>
 
 namespace edu { namespace vcccd { namespace vc { namespace csv15 {
+
+    int getMax (size_t array[], size_t n){
+        int max = array[0];
+        for(int i = 1; i < n; i++){
+            if (array[i] > max)
+                max = array[i];
+        }
+    }
 
     template<class T>
     void radixsort(T array[], size_t size) {
